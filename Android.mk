1e014ecc9ca1125fcdb5d9cb432e0191449418be
Michael Bestas <mkbestas@lineageos.org>
2023-12-05 18:05:58 +0200

    sargo: Import firmware from SP2A.220505.008
    Change-Id: Ibe2e18f6cf0158e5b784088606698b6160c226a9

--- Android.mk
+++ Android.mk
@@ -6,4 +6,17 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),sargo)
 
+$(call add-radio-file,radio/abl.img)
+$(call add-radio-file,radio/aop.img)
+$(call add-radio-file,radio/cmnlib.img)
+$(call add-radio-file,radio/cmnlib64.img)
+$(call add-radio-file,radio/devcfg.img)
+$(call add-radio-file,radio/hyp.img)
+$(call add-radio-file,radio/keymaster.img)
+$(call add-radio-file,radio/modem.img)
+$(call add-radio-file,radio/qupfw.img)
+$(call add-radio-file,radio/tz.img)
+$(call add-radio-file,radio/xbl.img)
+$(call add-radio-file,radio/xbl_config.img)
+
 endif
