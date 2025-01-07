74f8d630909bbc36572ddb8bd6800f16aa646fe6
Michael Bestas <mkbestas@lineageos.org>
2025-01-07 02:00:08 +0200

    sargo: Remove OMX blobs
    Change-Id: I49c594ab779bcbd149c80888e2df29ac9ab84101

--- sargo-vendor.mk
+++ sargo-vendor.mk
@@ -1016,21 +1016,12 @@ PRODUCT_PACKAGES += \
     sound_trigger.primary.sdm710 \
     vendor.qti.hardware.qteeconnector@1.0-impl \
     vulkan.adreno \
-    libAlacSwDec \
-    libApeSwDec \
     libC2D2 \
     libCB \
-    libFlacSwDec \
     libGPQTEEC_vendor \
     libGPTEE_vendor \
     libGPreqcancel \
     libGPreqcancel_svc \
-    libMpeg4SwEncoder \
-    libOmxAlacDecSw \
-    libOmxAmrwbplusDec \
-    libOmxApeDecSw \
-    libOmxEvrcDec \
-    libOmxQcelp13Dec \
     libOpenCL-pixel \
     libOpenCL \
     libQSEEComAPI \
@@ -1155,11 +1146,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wigig.netperftuner@1.0 \
     vendor.qti.power.pasrmanager@1.0 \
     vendor.qti.voiceprint@1.0 \
-    libOmxDsdDec \
     libadm \
     libadpcmdec \
     libcppf \
-    libdsd2pcm \
     libfastcrc \
     libfastcvdsp_stub \
     libfastcvopt \
